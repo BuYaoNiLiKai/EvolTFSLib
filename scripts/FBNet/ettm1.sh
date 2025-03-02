034bf77f05b4762e30734b2e5618f9f05ed39277
BuYaoNiLiKai <1923667524@qq.com>
2025-03-02 20:12:15 +0800

    时序预测库

---
+++ scripts/FBNet/ettm1.sh
@@ -0,0 +1,32 @@
+model_name=FBNet
+root_path_name=./dataset/
+data_path_name=ETTm1.csv
+model_id_name=ETTm1
+data_name=ETTm1
+seq_len=96
+for pred_len in 96 192 336 720
+do
+for random_seed in 2024
+do
+  python -u run.py \
+      --is_training 1 \
+      --root_path $root_path_name \
+      --data_path $data_path_name \
+      --model_id $model_id_name'_'$seq_len'_'$pred_len \
+      --model $model_name \
+      --data $data_name \
+      --features M \
+      --seq_len $seq_len \
+      --pred_len $pred_len \
+      --enc_in 7 \
+      --fb True \
+      --embed 'f'\
+      --des 'Exp' \
+      --itr 1 \
+      --freq 't' \
+      --batch_size 256 \
+      --lradj 'type3'\
+      --learning_rate 0.01 \
+      --random_seed $random_seed
+done
+done
